9402763ad6af77b61752289a577e0cd97da000c0
Laputa <huangguoxi@hotmail.com>
2023-11-10 13:54:58 +0800

    Init.

--- main_finetune.py
+++ main_finetune.py
@@ -582,6 +582,7 @@ def main(args):
 if __name__ == '__main__':
     args = get_args_parser()
     args = args.parse_args()
+    
     if args.output_dir:
         Path(args.output_dir).mkdir(parents=True, exist_ok=True)
     main(args)
